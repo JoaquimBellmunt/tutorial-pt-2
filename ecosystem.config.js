724bc19a681bc5b12615e62fd5199928acb209b7
Ubuntu <ubuntu@ip-172-31-34-218.ap-southeast-1.compute.internal>
2018-02-05 13:06:12 +0000

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,10 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-209-166-225.eu-west-1.compute.amazonaws.com',
-      key: '~/.ssh/tutorial-2.pem',
+      host: '54.169.212.231',
+      key: '~/.ssh/id_rsa',
       ref: 'origin/master',
-      repo: 'git@github.com:roberttod/tutorial-pt-2.git',
+      repo: 'git@github.com:JoaquimBellmunt/tutorial-pt-2.git',
       path: '/home/ubuntu/tutorial-2',
       'post-deploy': 'npm install && npm run-script restart'
     }
